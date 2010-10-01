9793d6f512766c1e839b1baa6dcd46d3d3e2aa2c
nebirhos <nebirhos@aol.com>
2010-10-01 02:42:17 +0200

    moved process logic into MassiveScrobbler module

--- lib/massive_scrobbler.rb
+++ lib/massive_scrobbler.rb
@@ -8,4 +8,20 @@ module MassiveScrobbler
   VERSION = "0.0.1" #:nodoc:
   CONFIG_PATH = File.expand_path '~/.massive-scrobbler'
 
+  def MassiveScrobbler.go!(args)
+    s = MassiveScrobbler::Songs.new(args[:path])
+    u = MassiveScrobbler::User.new(args[:verbose])
+
+    s.songs.each do |song|
+      mp3info = Mp3Info.open(song) do |mp3|
+        { :length => mp3.length.to_i,
+          :artist => mp3.tag.artist,
+          :title  => mp3.tag.title,
+          :album  => mp3.tag.album,
+          :trackn => mp3.tag.tracknum }
+      end
+      u.scrobble(mp3info)
+    end
+  end
+
 end
--- lib/massive_scrobbler/user.rb
+++ lib/massive_scrobbler/user.rb
@@ -3,37 +3,77 @@ require 'yaml'
 module MassiveScrobbler
 
   class User
-    attr_reader :user, :password
+    attr_reader :user
 
-    def initialize
+    def initialize(verbose=false)
+      @verbose = verbose
       if File.exists?(CONFIG_PATH)
-        load_user
+        load
+        authenticate
       else
-        puts "Hey! this seems the first time you launch this script"
+        puts "Hey! This seems the first time you launch this script."
         puts "In order to submit your tracks to LastFM, you have to login."
-        puts "This info will be stored in ~/.massive-scrobbler"
-        puts "Username:"
-        @user = gets.chomp
-        puts "Password:"
-        @password = gets.chomp
-        create_user(username, password)
+        puts "Please type your username and password (these will be stored in ~/.massive-scrobbler)."
+        prompt_for_credentials
+        while @user.empty? or @password.empty? or !authenticate
+          puts "Invalid login! Retry"
+          prompt_for_credentials
+        end
+        save
       end
     end
 
-    def create_user(user, password)
-      info = {:user => user, :password => password}
+    def prompt_for_credentials
+      puts "Username:"
+      @user = gets.chomp
+      puts "Password:"
+      @password = gets.chomp
+    end
+
+    def save
+      info = {:user => @user, :password => @password}
       File.open(CONFIG_PATH, 'w') do |conf|
         conf.write info.to_yaml
       end
     end
 
-    def load_user
+    def load
       info = {}
       File.open(CONFIG_PATH) do |conf|
         info = YAML::load(conf.read)
       end
-      @user = info[:user]
-      @password = info[:password]
+      @user, @password = info[:user], info[:password]
+    end
+
+    def authenticate
+      begin
+        @auth = Scrobbler::SimpleAuth.new(:user => @user, :password => @password)
+        @auth.handshake!
+        if @verbose
+          puts "Auth Status: #{@auth.status}"
+          puts "Session ID: #{@auth.session_id}"
+          puts "Now Playing URL: #{@auth.now_playing_url}"
+          puts "Submission URL: #{@auth.submission_url}"
+        end
+        true
+      rescue BadAuthError
+        nil
+      end
+    end
+
+    def scrobble(song)
+      scrobble = Scrobbler::Scrobble.new(:session_id => @auth.session_id,
+                                         :submission_url => @auth.submission_url,
+                                         :artist => song[:artist],
+                                         :track => song[:title],
+                                         :album => song[:album],
+                                         :time => Time.new,
+                                         :length => song[:length],
+                                         :track_number => song[:trackn])
+      scrobble.submit!
+      if @verbose
+        puts "#{song[:artist]} - #{song[:title]}: #{scrobble.status}"
+      end
     end
 
   end

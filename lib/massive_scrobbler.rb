e6d5dcd2582021444056ce41a53ce73d35ec97e9
nebirhos <nebirhos@aol.com>
2010-10-01 03:22:19 +0200

    added --delay option

--- lib/massive_scrobbler.rb
+++ lib/massive_scrobbler.rb
@@ -8,6 +8,7 @@ module MassiveScrobbler
   VERSION = "0.0.1" #:nodoc:
   CONFIG_PATH = File.expand_path '~/.massive-scrobbler'
 
+
   def MassiveScrobbler.go!(args)
     s = MassiveScrobbler::Songs.new(args[:path])
     u = MassiveScrobbler::User.new(args[:verbose])
@@ -20,8 +21,20 @@ module MassiveScrobbler
           :album  => mp3.tag.album,
           :trackn => mp3.tag.tracknum }
       end
+
       u.scrobble(mp3info)
+      if args[:delay] == 'real'
+        delay = mp3info[:length]
+      else
+        begin
+          delay = Float args[:delay]
+        rescue
+          delay = 0
+        end
+      end
+      sleep(delay)
     end
+
   end
 
 end
